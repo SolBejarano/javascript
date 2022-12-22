0959beed11987464e2b8843b178fc6fd611c4182
solbejarano <arq.msolbejaranogmail.com>
2022-12-21 22:00:10 -0300

    probando json

--- main.js
+++ main.js
@@ -126,35 +126,112 @@ console.log(busquedaSigno)*/
 en el prompt y en el this.name solamente me muestra el primer objeto, escriba el nombre del signo que escriba*/
 
 
-    let selectorDias = document.getElementById("dias")
-    for (let i = 1; i <= 31; i++) {
-        selectorDias.innerHTML += `<option value="${i}">${i}</option>`;
-    }
+let selectorDias = document.getElementById("dias")
+for (let i = 1; i <= 31; i++) {
+    selectorDias.innerHTML += `<option value="${i}">${i}</option>`;
+}
 
 
 const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
 
-let selectorMes = document.getElementById("meses");
+const selectorMes = document.getElementById("meses");
 selectorMes.innerHTML = "";
 meses.forEach((mes) => {
     selectorMes.innerHTML += `<option value="${mes}">${mes}</option>`;
 });
 
 
-let selectorAño = document.getElementById("año");
+const selectorAño = document.getElementById("año");
 let fecha = new Date();
 let año = fecha.getFullYear();
 for (let i = 1930; i <= año; i++) {
     selectorAño.innerHTML += `<option value="${i}">${i}</option>`;
 }
 
-const boton = document.getElementById ("btn") 
-let fechaNacimiento = selectorDias.value + selectorMes.value + selectorAño.value; 
-
-boton.onclik = (e) => {
-    console.log (fechaNacimiento)
-
+const selectorHora = document.getElementById("hora");
+for (let i = 0; i <= 23; i++) {
+    selectorHora.innerHTML += `<option value="${i}">${i}</option>`;
 }
 
+const selectorMinutos = document.getElementById("minutos");
+for (let i = 0; i < 60; i++) {
+    selectorMinutos.innerHTML += `<option value="${i}">${i}</option>`;
+}
+selectorMinutos.onChange = () => {
+    console.log(selectorMinutos.value)
+}
 
+const paises = ["Afganistán", "Albania", "Alemania", "Andorra", "Angola", "Antigua y Barbuda", "Arabia Saudita", "Argelia", "Argentina", "Armenia", "Australia", "Austria", "Azerbaiyán", "Bahamas", "Bangladés", "Barbados", "Baréin", "Bélgica", "Belice", "Benín", "Bielorrusia", "Birmania", "Bolivia", "Bosnia y Herzegovina", "Botsuana", "Brasil", "Brunéi", "Bulgaria", "Burkina Faso", "Burundi", "Bután", "Cabo Verde", "Camboya", "Camerún", "Canadá", "Catar", "Chad", "Chile", "China", "Chipre", "Ciudad del Vaticano", "Colombia", "Comoras", "Corea del Norte", "Corea del Sur", "Costa de Marfil", "Costa Rica", "Croacia", "Cuba", "Dinamarca", "Dominica", "Ecuador", "Egipto", "El Salvador", "Emiratos Árabes Unidos", "Eritrea", "Eslovaquia", "Eslovenia", "España", "Estados Unidos", "Estonia", "Etiopía", "Filipinas", "Finlandia", "Fiyi", "Francia", "Gabón", "Gambia", "Georgia", "Ghana", "Granada", "Grecia", "Guatemala", "Guyana", "Guinea", "Guinea ecuatorial", "Guinea-Bisáu", "Haití", "Honduras", "Hungría", "India", "Indonesia", "Irak", "Irán", "Irlanda", "Islandia", "Islas Marshall", "Islas Salomón", "Israel", "Italia", "Jamaica", "Japón", "Jordania", "Kazajistán", "Kenia", "Kirguistán", "Kiribati", "Kuwait", "Laos", "Lesoto", "Letonia", "Líbano", "Liberia", "Libia", "Liechtenstein", "Lituania", "Luxemburgo", "Madagascar", "Malasia", "Malaui", "Maldivas", "Malí", "Malta", "Marruecos", "Mauricio", "Mauritania", "México", "Micronesia", "Moldavia", "Mónaco", "Mongolia", "Montenegro", "Mozambique", "Namibia", "Nauru", "Nepal", "Nicaragua", "Níger", "Nigeria", "Noruega", "Nueva Zelanda", "Omán", "Países Bajos", "Pakistán", "Palaos", "Palestina", "Panamá", "Papúa Nueva Guinea", "Paraguay", "Perú", "Polonia", "Portugal", "Reino Unido", "República Centroafricana", "República Checa", "República de Macedonia", "República del Congo", "República Democrática del Congo", "República Dominicana", "República Sudafricana", "Ruanda", "Rumanía", "Rusia", "Samoa", "San Cristóbal y Nieves", "San Marino", "San Vicente y las Granadinas", "Santa Lucía", "Santo Tomé y Príncipe", "Senegal", "Serbia", "Seychelles", "Sierra Leona", "Singapur", "Siria", "Somalia", "Sri Lanka", "Suazilandia", "Sudán", "Sudán del Sur", "Suecia", "Suiza", "Surinam", "Tailandia", "Tanzania", "Tayikistán", "Timor Oriental", "Togo", "Tonga", "Trinidad y Tobago", "Túnez", "Turkmenistán", "Turquía", "Tuvalu", "Ucrania", "Uganda", "Uruguay", "Uzbekistán", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Yibuti", "Zambia", "Zimbabue"]
 
+let selectorPaises = document.getElementById("pais")
+
+paises.forEach((pais) => {
+    selectorPaises.innerHTML += `<option value="${pais}">${pais}</option>`;
+});
+
+const form = document.getElementById("form");
+const boton = document.getElementById("btn");
+
+const input = document.getElementById("input");
+
+input.addEventListener("change", () => { console.log(`${input.value}`) })
+
+selectorDias.onchange = () => {
+
+    console.log(selectorDias.value)
+}
+let diaNacimiento = selectorDias.value
+
+selectorMes.onchange = () => {
+    console.log(selectorMes.value)
+}
+let mesNacimiento = selectorMes.value
+selectorAño.onchange = () => {
+    console.log(selectorAño.value)
+}
+
+/* Para poder "recolectar" la info de los usuarios creo un usuario que va a estar en un array de usuarios*/
+
+class usuario {
+    constructor(nombre, diaNacimiento, mesNacimiento, añoNacimiento, horaNacimiento, minutosNacimiento) {
+
+        this.nombre = nombre;
+        this.diaNacimiento = diaNacimiento;
+        this.mesNacimiento = mesNacimiento;
+        this.añoNacimiento = añoNacimiento;
+        this.horaNacimiento = horaNacimiento;
+        this.minutosNacimiento = minutosNacimiento;
+    }
+}
+
+const usuarios = [];
+
+if (localStorage.getItem`usuarios`) {
+    let usuario = JSON.parse(localStorage.getItem(`usuarios`));
+    for (let i = 0; i < usuario.length; i++) {
+        usuarios.push(usuario[i]);
+    }
+}
+
+form.onclick = (e) => {
+    e.preventDefault();
+    agregarUsuario();
+}
+
+/* En esta parte de la función tengo mis dudas, acá tengo el problema 
+que me borra los datos del nombre, y no me selecciona la info en los
+selectores*/ 
+
+function agregarUsuario() {
+    const nombre = document.getElementById("input").value;
+    selectorMes.onchange = () => { 
+        const mesNacimiento= selectorMes.value 
+    }
+   selectorAño.onchange = () => {
+    const añoNacimiento= selectorAño.value;}
+    const horaNacimiento = selectorHora.value;
+    const minutosNacimiento = selectorMinutos.value;
+    const nuevoUsuario = (nombre, mesNacimiento, añoNacimiento, horaNacimiento, minutosNacimiento)
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+    form.reset();
+}
